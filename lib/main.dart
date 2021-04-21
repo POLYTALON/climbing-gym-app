593da8f371bf08bb792f85d1f867f242832f824f
v1lling <sascha.villing@web.de>
2021-04-21 10:46:58 +0200

    Initialize back4app connection and .env file

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
 import 'package:climbing_gym_app/screens/start.dart';
 import 'package:flutter/material.dart';
+import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart' as DotEnv;
 
-void main() {
+void main() async {
+  await DotEnv.load();
+  final keyApplicationId = DotEnv.env['BACK4APP_APPLICATIONID'];
+  final keyClientKey = DotEnv.env['BACK4APP_CLIENTSECRET'];
+  final keyParseServerUrl = 'https://parseapi.back4app.com';
+
+  await Parse().initialize(keyApplicationId, keyParseServerUrl,
+      clientKey: keyClientKey, autoSendSessionId: true);
+
   runApp(MyApp());
 }
 
