dc0fa38cf13bee23c7c406b49b366ef7bef07b32
v1lling <sascha.villing@web.de>
2021-04-26 16:28:37 +0200

    Add validators for login form

---
+++ lib/validators/email_validator.dart
@@ -0,0 +1,14 @@
+class EmailFieldValidator {
+  static String validate(String value) {
+    if (!isValidEmail(value) || value.isEmpty) {
+      return 'Email is invalid';
+    }
+    return null;
+  }
+
+  static bool isValidEmail(String email) {
+    String p = r"^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+";
+    RegExp regExp = new RegExp(p);
+    return regExp.hasMatch(email);
+  }
+}
