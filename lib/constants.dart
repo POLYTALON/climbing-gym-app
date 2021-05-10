0039cc68822d517a1e5aa9b754e531908c0f3157
BernhardGundel <bernhard.gundel@htwg-konstanz.de>
2021-05-10 15:19:01 +0200

    Added sliding_up_panel dependency. Added SlidingUpPanel in GymsScreen without content for now.

--- lib/constants.dart
+++ lib/constants.dart
@@ -5,3 +5,4 @@ const Color polyGreen = Color(0xFF00bb56);
 const Color polyDark = Color(0xFF121212);
 const Color polyRed = Color(0xFFbb0000);
 const Color polyGray = Color(0xFF414141);
+const Color lightGray = Color(0xFF808080);
--- lib/screens/gyms.dart
+++ lib/screens/gyms.dart
@@ -3,56 +3,99 @@ import 'package:climbing_gym_app/services/gymsService.dart';
 import 'package:climbing_gym_app/widgets/gymCard.dart';
 import 'package:climbing_gym_app/constants.dart' as Constants;
 import 'package:flutter/material.dart';
+import 'package:flutter_sliding_up_panel/flutter_sliding_up_panel.dart';
 import 'package:provider/provider.dart';
 
 class GymsScreen extends StatelessWidget {
+  // The controller of the sliding panel
+  final SlidingUpPanelController _panelController = SlidingUpPanelController();
+
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
     final double itemHeight = (size.height - kToolbarHeight - 24) / 3;
     final double itemWidth = size.width / 2;
 
-    return Scaffold(
-        floatingActionButton: FloatingActionButton(
-          child: const Icon(Icons.add),
-          backgroundColor: Constants.polyGreen,
-          onPressed: () {},
-        ),
-        backgroundColor: Constants.polyDark,
-        body: Container(
-            child: ChangeNotifierProvider<GymsService>(
-                create: (context) => GymsService(),
-                child: Consumer<GymsService>(
-                    builder: (context, gymsService, child) {
-                  return Column(children: [
-                    // Text
-                    Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Text("Choose your Gym",
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.w700,
-                              fontSize: 24)),
-                    ),
+    return Stack(children: <Widget>[
+      Scaffold(
+          // Add gym button
+          floatingActionButton: FloatingActionButton(
+            child: const Icon(Icons.add),
+            backgroundColor: Constants.polyGreen,
+            onPressed: () => toggleSlidingPanel(),
+          ),
+          backgroundColor: Constants.polyDark,
 
-                    // GridView (with GymCards)
-                    Expanded(
-                      child: GridView.count(
-                          crossAxisCount: 2,
-                          childAspectRatio: (itemWidth / itemHeight),
-                          children: List.generate(gymsService.gymsList.length,
-                              (index) {
-                            return Container(
-                                child: GymCard(
-                                    gym: Gym(
-                                        name:
-                                            '${gymsService.gymsList[index].name}',
-                                        imageUrl:
-                                            '${gymsService.gymsList[index].imageUrl}')));
-                          })),
-                    )
-                  ]);
-                }))));
+          // Page content
+          body: Container(
+              child: ChangeNotifierProvider<GymsService>(
+                  create: (context) => GymsService(),
+                  child: Consumer<GymsService>(
+                      builder: (context, gymsService, child) {
+                    return Column(children: [
+                      // Text
+                      Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Text("Choose your Gym",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w700,
+                                fontSize: 24)),
+                      ),
+
+                      // GridView (with GymCards)
+                      Expanded(
+                        child: GridView.count(
+                            crossAxisCount: 2,
+                            childAspectRatio: (itemWidth / itemHeight),
+                            children: List.generate(gymsService.gymsList.length,
+                                (index) {
+                              return Container(
+                                  child: GymCard(
+                                      gym: Gym(
+                                          name:
+                                              '${gymsService.gymsList[index].name}',
+                                          imageUrl:
+                                              '${gymsService.gymsList[index].imageUrl}')));
+                            })),
+                      )
+                    ]);
+                  })))),
+
+      // SlidingUpPanelWidget
+      SlidingUpPanelWidget(
+          child: Container(
+            // margin: EdgeInsets.symmetric(horizontal: 15.0),
+            decoration: ShapeDecoration(
+              color: Constants.lightGray,
+              shadows: [
+                BoxShadow(
+                    blurRadius: 5.0,
+                    spreadRadius: 2.0,
+                    color: const Color(0x11000000))
+              ],
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(16.0),
+                      topRight: Radius.circular(16.0))),
+            ),
+          ),
+          controlHeight: 0.0,
+          anchor: 0.6,
+          panelController: _panelController,
+          enableOnTap: false,
+          onTap: () {
+            toggleSlidingPanel();
+          })
+    ]);
+  }
+
+  void toggleSlidingPanel() {
+    if (SlidingUpPanelStatus.expanded == _panelController.status) {
+      _panelController.collapse();
+    } else {
+      _panelController.anchor();
+    }
   }
 }
