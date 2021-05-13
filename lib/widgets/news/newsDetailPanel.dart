01b49b896a3c617f3c1daf43a375f4b92230e08d
v1lling <sascha.villing@web.de>
2021-05-13 17:37:01 +0200

    Change News Detail Screen

--- lib/widgets/news/newsDetailPanel.dart
+++ lib/widgets/news/newsDetailPanel.dart
@@ -36,79 +36,85 @@ class _NewsDetailPanelState extends State<NewsDetailPanel> {
     });
 
     return Container(
+      constraints: BoxConstraints.expand(),
       padding: EdgeInsets.only(left: 16, right: 16),
       child: SlidingUpPanelWidget(
-          controlHeight: 1.0,
-          anchor: 1.0,
-          panelController: _panelController,
-          child: SingleChildScrollView(
-            child: Container(
-              decoration: ShapeDecoration(
-                color: Colors.white,
-                shadows: [
-                  BoxShadow(
-                      blurRadius: 8.0,
-                      spreadRadius: 16.0,
-                      color: const Color(0x11000000))
-                ],
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(16.0),
-                        topRight: Radius.circular(16.0))),
-              ),
+        controlHeight: 1.0,
+        anchor: 1.0,
+        panelController: _panelController,
+        child: Container(
+          decoration: ShapeDecoration(
+            color: Colors.white,
+            shadows: [
+              BoxShadow(
+                  blurRadius: 8.0,
+                  spreadRadius: 16.0,
+                  color: const Color(0x11000000))
+            ],
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(16.0),
+                    topRight: Radius.circular(16.0))),
+          ),
 
-              // SlidingUpPanel content
-              child: Padding(
-                padding: const EdgeInsets.only(left: 16.0, right: 16.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
+          // SlidingUpPanel content
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.only(
+                  top: 8, bottom: 8, left: 16.0, right: 16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Flexible(
+                        child: Text(
                           newsProvider.currentNewsDetails.title ?? "",
                           style: Constants.headerText,
                         ),
-                        IconButton(
-                            onPressed: () {
-                              this._panelController.collapse();
-                            },
-                            icon: const Icon(Icons.close)),
-                      ],
-                    ),
-                    Container(
-                        padding: const EdgeInsets.only(
-                          bottom: 16.0,
-                        ),
-                        constraints:
-                            BoxConstraints(minHeight: 100, maxHeight: 200),
-                        child: Stack(
-                          children: <Widget>[
-                            Center(child: CircularProgressIndicator()),
-                            Center(
-                              child: FadeInImage.memoryNetwork(
-                                  placeholder: kTransparentImage,
-                                  image: newsProvider.currentNews.imageUrls !=
-                                          null
-                                      ? newsProvider.currentNews.imageUrls[0]
-                                      : ""),
-                            ),
-                          ],
-                        )),
-                    Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(newsProvider.currentNews.subtitle ?? "",
-                              style: Constants.subHeaderText),
-                          Text(newsProvider.currentNews.content ?? "",
-                              style: Constants.defaultText)
-                        ])
-                  ],
-                ),
+                      ),
+                      IconButton(
+                          onPressed: () {
+                            this._panelController.collapse();
+                          },
+                          icon: const Icon(Icons.close)),
+                    ],
+                  ),
+                  Container(
+                      padding: const EdgeInsets.only(
+                        top: 8,
+                        bottom: 16.0,
+                      ),
+                      constraints:
+                          BoxConstraints(minHeight: 100, maxHeight: 200),
+                      child: Stack(
+                        children: <Widget>[
+                          Center(child: CircularProgressIndicator()),
+                          Center(
+                            child: FadeInImage.memoryNetwork(
+                                placeholder: kTransparentImage,
+                                image:
+                                    newsProvider.currentNews.imageUrls != null
+                                        ? newsProvider.currentNews.imageUrls[0]
+                                        : ""),
+                          ),
+                        ],
+                      )),
+                  Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(newsProvider.currentNews.subtitle ?? "",
+                            style: Constants.subHeaderText),
+                        Text(newsProvider.currentNews.content ?? "",
+                            style: Constants.defaultText)
+                      ]),
+                ],
               ),
             ),
-          )),
+          ),
+        ),
+      ),
     );
   }
 }
