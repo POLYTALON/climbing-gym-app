67ba638b478813c18d2b96ded2b09e578a4f5425
BernhardGundel <bernhard.gundel@htwg-konstanz.de>
2021-05-31 11:21:34 +0200

    Tried some stuff. Not working.

---
+++ lib/models/AppRoute.dart
@@ -0,0 +1,62 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:equatable/equatable.dart';
+
+class AppRoute extends Equatable {
+  final String id;
+  final String name;
+  final String type;
+  //final List<Map<String, DateTime>> comments;
+  //final List<int> rating;
+  final List<String> builders;
+  final DateTime date;
+  final String difficulty;
+  final String gymId;
+  final String holds;
+  final List<String> imageUrls;
+
+  @override
+  List<Object> get props => [
+        id,
+        name,
+        type,
+        //comments,
+        //rating,
+        builders,
+        date,
+        difficulty,
+        gymId,
+        holds,
+        imageUrls
+      ];
+
+  AppRoute(
+      {this.id,
+      this.name,
+      this.type,
+      //this.comments,
+      //this.rating,
+      this.builders,
+      this.date,
+      this.difficulty,
+      this.gymId,
+      this.holds,
+      this.imageUrls});
+
+  factory AppRoute.fromFirestore(DocumentSnapshot doc) {
+    Map docData = doc.data();
+    print(docData);
+    // Map ratingsData = ratings.data();
+    return AppRoute(
+        id: doc.id ?? '',
+        name: docData['name'] ?? '',
+        builders: docData['builders'] ?? [],
+        date: docData['date'] ?? DateTime.now(),
+        difficulty: docData['difficulty'] ?? '',
+        gymId: docData['gymid'] ?? '',
+        holds: docData['holds'] ?? '',
+        type: docData['type'] ?? '',
+        imageUrls: docData['imageUrls'] ?? []);
+    //comments: ratingsData['comments'] ?? [],
+    //rating: ratingsData['rating'] ?? []);
+  }
+}
