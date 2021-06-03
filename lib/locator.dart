5e846d8c9c690844ae7189a99d8bb031879080aa
Sascha Villing <sa981vil@htwg-konstanz.de>
2021-06-03 11:14:34 +0200

    Extend routesService

--- lib/locator.dart
+++ lib/locator.dart
@@ -1,4 +1,4 @@
-import 'package:climbing_gym_app/services/RoutesService.dart';
+import 'package:climbing_gym_app/services/routesService.dart';
 import 'package:climbing_gym_app/services/gymService.dart';
 import 'package:get_it/get_it.dart';
 
