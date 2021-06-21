f736e1bd67be8d20d31038c7fd92e45b656ea88b
Sascha Villing <sa981vil@htwg-konstanz.de>
2021-06-21 15:30:08 +0200

    revert fix

--- lib/widgets/routes/routeEditPanel.dart
+++ lib/widgets/routes/routeEditPanel.dart
@@ -153,7 +153,6 @@ class _RouteEditPanelState extends State<RouteEditPanel> with GetItStateMixin {
                                                 (newImage) {
                                               if (newImage != null) {
                                                 setState(() {
-                                                  imageCache.clear();
                                                   _image = newImage;
                                                 });
                                               }
