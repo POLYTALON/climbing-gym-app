cc5f917afb42260068892f32d18e19d5754739ac
Sascha Villing <sa981vil@htwg-konstanz.de>
2021-06-28 15:26:48 +0200

    Fix keyboard hide textfield bug

--- lib/screens/auth/register.dart
+++ lib/screens/auth/register.dart
@@ -24,188 +24,193 @@ class _RegisterScreenState extends State<RegisterScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      color: Constants.polyDark,
-      child: Container(
-        margin: const EdgeInsets.only(left: 64.0, right: 64.0, top: 32),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            // Polytalon Logo
-            Image.asset('assets/img/polytalon_logo.png'),
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
+      backgroundColor: Constants.polyDark,
+      body: SingleChildScrollView(
+        child: Container(
+          margin: const EdgeInsets.only(left: 64.0, right: 64.0, top: 32),
+          height: MediaQuery.of(context).size.height,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              // Polytalon Logo
+              Image.asset('assets/img/polytalon_logo.png'),
 
-            // Spacer
-            Spacer(flex: 1),
+              // Spacer
+              Spacer(flex: 1),
 
-            Form(
-              key: _formKey,
-              child: Column(
-                children: [
-                  // Text Field Username
-                  Padding(
-                    padding: const EdgeInsets.only(left: 16.0, bottom: 4.0),
-                    child: Text("Name:", style: TextStyle(color: Colors.white)),
-                  ),
-                  TextFormField(
-                      controller: controllerDisplayName,
-                      enabled: !isLoggedIn,
-                      autocorrect: false,
-                      textCapitalization: TextCapitalization.words,
-                      style: TextStyle(fontWeight: FontWeight.w800),
-                      // Provides a keyboard optimized for typing in names
-                      keyboardType: TextInputType.name,
-                      validator: NameFieldValidator.validate,
-                      decoration: InputDecoration(
-                          contentPadding: const EdgeInsets.only(left: 16.0),
-                          hintText: 'Max Mustermann',
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(24.0),
-                              borderSide: BorderSide(
-                                  width: 0, style: BorderStyle.none)),
-                          fillColor: Colors.white,
-                          filled: true)),
+              Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    // Text Field Username
+                    Padding(
+                      padding: const EdgeInsets.only(left: 16.0, bottom: 4.0),
+                      child:
+                          Text("Name:", style: TextStyle(color: Colors.white)),
+                    ),
+                    TextFormField(
+                        controller: controllerDisplayName,
+                        enabled: !isLoggedIn,
+                        autocorrect: false,
+                        textCapitalization: TextCapitalization.words,
+                        style: TextStyle(fontWeight: FontWeight.w800),
+                        // Provides a keyboard optimized for typing in names
+                        keyboardType: TextInputType.name,
+                        validator: NameFieldValidator.validate,
+                        decoration: InputDecoration(
+                            contentPadding: const EdgeInsets.only(left: 16.0),
+                            hintText: 'Max Mustermann',
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(24.0),
+                                borderSide: BorderSide(
+                                    width: 0, style: BorderStyle.none)),
+                            fillColor: Colors.white,
+                            filled: true)),
 
-                  // Text Field E-Mail
-                  Padding(
-                    padding: const EdgeInsets.only(left: 16.0, bottom: 4.0),
-                    child: Text("E-Mail-Address:",
-                        style: TextStyle(color: Colors.white)),
-                  ),
-                  TextFormField(
-                      controller: controllerEmail,
-                      enabled: !isLoggedIn,
-                      autocorrect: false,
-                      textCapitalization: TextCapitalization.none,
-                      style: TextStyle(fontWeight: FontWeight.w800),
-                      // it's a text field to type in an email-address, duh!
-                      keyboardType: TextInputType.emailAddress,
-                      validator: EmailFieldValidator.validate,
-                      decoration: InputDecoration(
-                          contentPadding: const EdgeInsets.only(left: 16.0),
-                          hintText: 'max.mustermann@polytalon.com',
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(24.0),
-                              borderSide: BorderSide(
-                                  width: 0, style: BorderStyle.none)),
-                          fillColor: Colors.white,
-                          filled: true)),
+                    // Text Field E-Mail
+                    Padding(
+                      padding: const EdgeInsets.only(left: 16.0, bottom: 4.0),
+                      child: Text("E-Mail-Address:",
+                          style: TextStyle(color: Colors.white)),
+                    ),
+                    TextFormField(
+                        controller: controllerEmail,
+                        enabled: !isLoggedIn,
+                        autocorrect: false,
+                        textCapitalization: TextCapitalization.none,
+                        style: TextStyle(fontWeight: FontWeight.w800),
+                        // it's a text field to type in an email-address, duh!
+                        keyboardType: TextInputType.emailAddress,
+                        validator: EmailFieldValidator.validate,
+                        decoration: InputDecoration(
+                            contentPadding: const EdgeInsets.only(left: 16.0),
+                            hintText: 'max.mustermann@polytalon.com',
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(24.0),
+                                borderSide: BorderSide(
+                                    width: 0, style: BorderStyle.none)),
+                            fillColor: Colors.white,
+                            filled: true)),
 
-                  // Text Field Password
-                  Padding(
-                    padding: const EdgeInsets.only(
-                        left: 16.0, top: 4.0, bottom: 4.0),
-                    child: Text("Password:",
-                        style: TextStyle(color: Colors.white)),
-                  ),
-                  TextFormField(
-                      controller: controllerPassword,
-                      enabled: !isLoggedIn,
-                      textCapitalization: TextCapitalization.none,
-                      style: TextStyle(fontWeight: FontWeight.w800),
-                      obscureText: true,
-                      enableSuggestions: false,
-                      autocorrect: false,
-                      // it's a text field to type in a password, duh!
-                      keyboardType: TextInputType.visiblePassword,
-                      validator: PasswordFieldValidator.validate,
-                      decoration: InputDecoration(
-                          contentPadding: const EdgeInsets.only(left: 16.0),
-                          hintText: '********',
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(24.0),
-                              borderSide: BorderSide(
-                                  width: 0, style: BorderStyle.none)),
-                          fillColor: Colors.white,
-                          filled: true)),
+                    // Text Field Password
+                    Padding(
+                      padding: const EdgeInsets.only(
+                          left: 16.0, top: 4.0, bottom: 4.0),
+                      child: Text("Password:",
+                          style: TextStyle(color: Colors.white)),
+                    ),
+                    TextFormField(
+                        controller: controllerPassword,
+                        enabled: !isLoggedIn,
+                        textCapitalization: TextCapitalization.none,
+                        style: TextStyle(fontWeight: FontWeight.w800),
+                        obscureText: true,
+                        enableSuggestions: false,
+                        autocorrect: false,
+                        // it's a text field to type in a password, duh!
+                        keyboardType: TextInputType.visiblePassword,
+                        validator: PasswordFieldValidator.validate,
+                        decoration: InputDecoration(
+                            contentPadding: const EdgeInsets.only(left: 16.0),
+                            hintText: '********',
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(24.0),
+                                borderSide: BorderSide(
+                                    width: 0, style: BorderStyle.none)),
+                            fillColor: Colors.white,
+                            filled: true)),
 
-                  // Text Field Password (repeat)
-                  Padding(
-                    padding: const EdgeInsets.only(
-                        left: 16.0, top: 4.0, bottom: 4.0),
-                    child: Text("Repeat password:",
-                        style: TextStyle(color: Colors.white)),
-                  ),
-                  TextFormField(
-                      controller: controllerPasswordRepeat,
-                      enabled: !isLoggedIn,
-                      textCapitalization: TextCapitalization.none,
-                      style: TextStyle(fontWeight: FontWeight.w800),
-                      obscureText: true,
-                      enableSuggestions: false,
-                      autocorrect: false,
-                      // it's a text field to type in a password, duh!
-                      keyboardType: TextInputType.visiblePassword,
-                      validator: PasswordFieldValidator.validate,
-                      decoration: InputDecoration(
-                          contentPadding: const EdgeInsets.only(left: 16.0),
-                          hintText: '********',
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(24.0),
-                              borderSide: BorderSide(
-                                  width: 0, style: BorderStyle.none)),
-                          fillColor: Colors.white,
-                          filled: true)),
-                ],
+                    // Text Field Password (repeat)
+                    Padding(
+                      padding: const EdgeInsets.only(
+                          left: 16.0, top: 4.0, bottom: 4.0),
+                      child: Text("Repeat password:",
+                          style: TextStyle(color: Colors.white)),
+                    ),
+                    TextFormField(
+                        controller: controllerPasswordRepeat,
+                        enabled: !isLoggedIn,
+                        textCapitalization: TextCapitalization.none,
+                        style: TextStyle(fontWeight: FontWeight.w800),
+                        obscureText: true,
+                        enableSuggestions: false,
+                        autocorrect: false,
+                        // it's a text field to type in a password, duh!
+                        keyboardType: TextInputType.visiblePassword,
+                        validator: PasswordFieldValidator.validate,
+                        decoration: InputDecoration(
+                            contentPadding: const EdgeInsets.only(left: 16.0),
+                            hintText: '********',
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(24.0),
+                                borderSide: BorderSide(
+                                    width: 0, style: BorderStyle.none)),
+                            fillColor: Colors.white,
+                            filled: true)),
+                  ],
+                ),
               ),
-            ),
 
-            // Spacer
-            Spacer(flex: 1),
+              // Spacer
+              Spacer(flex: 1),
 
-            // Error Message
-            Center(
-                child: Text(_errorMessage,
+              // Error Message
+              Center(
+                  child: Text(_errorMessage,
+                      style: TextStyle(
+                          color: Colors.red, fontWeight: FontWeight.w800))),
+
+              // Spacer
+              Spacer(flex: 1),
+
+              // Button Register
+              ElevatedButton(
+                style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all(Constants.polyGreen),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(24.0)),
+                    )),
+                onPressed: isLoggedIn ? null : () => doUserRegistration(),
+                child: Text("Register",
                     style: TextStyle(
-                        color: Colors.red, fontWeight: FontWeight.w800))),
-
-            // Spacer
-            Spacer(flex: 1),
-
-            // Button Register
-            ElevatedButton(
-              style: ButtonStyle(
-                  backgroundColor:
-                      MaterialStateProperty.all(Constants.polyGreen),
-                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                    RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(24.0)),
-                  )),
-              onPressed: isLoggedIn ? null : () => doUserRegistration(),
-              child: Text("Register",
-                  style: TextStyle(
-                      color: Colors.white, fontWeight: FontWeight.w900)),
-            ),
-
-            // Spacer
-            Spacer(),
-
-            // Spacer
-            Spacer(flex: 2),
-
-            // Goto Login Button
-            Text(
-              "You are already signed up?",
-              style: TextStyle(
-                color: Colors.white,
+                        color: Colors.white, fontWeight: FontWeight.w900)),
               ),
-              textAlign: TextAlign.center,
-            ),
-            ElevatedButton(
-              style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(Colors.white),
-                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                    RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(24.0)),
-                  )),
-              onPressed: () => navigateToLogin(),
-              child: Text("Sign In",
-                  style: TextStyle(
-                      color: Colors.black, fontWeight: FontWeight.w900)),
-            ),
 
-            // Spacer
-            Spacer(flex: 2),
-          ],
+              // Spacer
+              Spacer(),
+
+              // Spacer
+              Spacer(flex: 2),
+
+              // Goto Login Button
+              Text(
+                "You are already signed up?",
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              ElevatedButton(
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.white),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(24.0)),
+                    )),
+                onPressed: () => navigateToLogin(),
+                child: Text("Sign In",
+                    style: TextStyle(
+                        color: Colors.black, fontWeight: FontWeight.w900)),
+              ),
+
+              // Spacer
+              Spacer(flex: 2),
+            ],
+          ),
         ),
       ),
     );
