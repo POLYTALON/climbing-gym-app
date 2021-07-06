d9311c9ac57113e7ec95c4b0dfe2dae036539e2f
Sascha Villing <sa981vil@htwg-konstanz.de>
2021-07-06 17:20:29 +0200

    add picture to about page

--- lib/screens/about.dart
+++ lib/screens/about.dart
@@ -45,50 +45,62 @@ class AboutScreen extends StatelessWidget {
                   margin: const EdgeInsets.only(left: 19.0, right: 19.0),
                 ),
               ))),
-      body: Center(
-        child: Container(
-          margin: const EdgeInsets.only(
-              left: 19.0, right: 19.0, top: 30.0, bottom: 50.0),
-          constraints: BoxConstraints.expand(),
-          child:
-              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-            Padding(
-              padding: const EdgeInsets.only(
-                  left: 18.0, right: 18.0, top: 18.0, bottom: 10.0),
-              child: Text(
-                  "INNOVATIONS THROUGHOUT\nTHE VALUE CHAIN FOR YOUR\nADVANTAGES",
-                  textAlign: TextAlign.left,
-                  style: TextStyle(
-                      fontSize: 14,
-                      fontWeight: FontWeight.w900,
-                      color: Colors.white)),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 18.0, right: 18.0),
-              child: PreferredSize(
-                preferredSize: Size.fromHeight(2.0),
-                child: Container(
-                  color: Colors.white38,
-                  height: 2.0,
-                  margin: const EdgeInsets.only(left: 0.0, right: 0.0),
+      body: Container(
+        margin: const EdgeInsets.only(
+            left: 19.0, right: 19.0, top: 30.0, bottom: 50.0),
+        constraints: BoxConstraints.expand(),
+        child: Column(
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: 18.0, right: 18.0, top: 18.0, bottom: 10.0),
+                child: Text(
+                    "INNOVATIONS THROUGHOUT\nTHE VALUE CHAIN FOR YOUR\nADVANTAGES",
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w900,
+                        color: Colors.white)),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 18.0, right: 18.0),
+                child: PreferredSize(
+                  preferredSize: Size.fromHeight(2.0),
+                  child: Container(
+                    color: Colors.white38,
+                    height: 2.0,
+                    margin: const EdgeInsets.only(left: 0.0, right: 0.0),
+                  ),
                 ),
               ),
-            ),
-            Padding(
-              padding:
-                  const EdgeInsets.only(left: 18.0, right: 18.0, top: 15.0),
-              child: Text(
-                  "POLYTALON produces climbing holds in a ground-breaking way: Automated, fast and sustainable. Local production for reasonable prices – realised by outstanding engineering. \n\nOur long lasting silicon molds (patent pending)save material and provide a fully automated demoulding – Essential when it comes to production in the heart of europe. \n\nMade-to-measure PU resins ensure high durabilty and ultimate grip of our climbing holds. Preventing breakage of edges maximizing lifetime at the wall. \n\nLess waste and less production time. POLYTALON ensures highest production standards putting the customer first.",
-                  style: TextStyle(
-                      fontSize: 11,
-                      fontWeight: FontWeight.w900,
-                      color: Colors.white)),
-            ),
-          ]),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(6),
-            color: Constants.polyGray,
-          ),
+              Padding(
+                padding:
+                    const EdgeInsets.only(left: 18.0, right: 18.0, top: 15.0),
+                child: Text(
+                    "POLYTALON produces climbing holds in a ground-breaking way: Automated, fast and sustainable. Local production for reasonable prices – realised by outstanding engineering. \n\nOur long lasting silicon molds (patent pending)save material and provide a fully automated demoulding – Essential when it comes to production in the heart of europe. \n\nMade-to-measure PU resins ensure high durabilty and ultimate grip of our climbing holds. Preventing breakage of edges maximizing lifetime at the wall. \n\nLess waste and less production time. POLYTALON ensures highest production standards putting the customer first.",
+                    style: TextStyle(
+                        fontSize: 11,
+                        fontWeight: FontWeight.w900,
+                        color: Colors.white)),
+              ),
+              Expanded(
+                child: Center(
+                  child: Container(
+                      margin: const EdgeInsets.all(16),
+                      decoration: new BoxDecoration(
+                          image: new DecorationImage(
+                              image: new AssetImage(
+                                'assets/img/poly_hold_image.jpg',
+                              ),
+                              fit: BoxFit.cover))),
+                ),
+              )
+            ]),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(6),
+          color: Constants.polyGray,
         ),
       ),
     );
