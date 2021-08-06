83b156e42c3f853de07df90c39bbcc6c39856970
Sascha Villing <sa981vil@htwg-konstanz.de>
2021-08-06 15:51:57 +0200

    Align user settings icon

--- lib/widgets/home/profileCard.dart
+++ lib/widgets/home/profileCard.dart
@@ -40,135 +40,158 @@ class _ProfileCardState extends State<ProfileCard> {
   @override
   Widget build(BuildContext context) {
     return Container(
-        child: Card(
-            clipBehavior: Clip.antiAlias,
-            color: Constants.polyGray,
-            shape:
-                RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
-            elevation: 2,
-            child: Padding(
-                padding: EdgeInsets.all(16.0),
+      child: Card(
+        clipBehavior: Clip.antiAlias,
+        color: Constants.polyGray,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+        elevation: 2,
+        child: Padding(
+          padding: EdgeInsets.fromLTRB(16, 0, 8, 16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Expanded(
+                      flex: 3,
+                      child: Container(
+                          padding: EdgeInsets.only(top: 16),
+                          alignment: Alignment.centerLeft,
+                          child: CircleAvatar(
+                              minRadius: 16.0,
+                              maxRadius: 32.0,
+                              //backgroundImage: NetworkImage(appUser.imageUrl),
+                              child: AutoSizeText(
+                                  _getUserInitials(appUser.displayName),
+                                  maxLines: 1,
+                                  style: Constants.subHeaderTextWhite))),
+                    ),
+                    Expanded(
+                        flex: 7,
+                        child: Container(
+                          padding: EdgeInsets.only(left: 8, top: 16),
+                          child: FittedBox(
+                              fit: BoxFit.fitWidth,
+                              child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceBetween,
+                                        mainAxisSize: MainAxisSize.max,
+                                        children: <Widget>[
+                                          AutoSizeText(
+                                            appUser.displayName ?? '',
+                                            maxLines: 1,
+                                            style: Constants.headerTextWhite,
+                                            textAlign: TextAlign.left,
+                                          ),
+                                        ]),
+                                    AutoSizeText(
+                                      appUser.email ?? '',
+                                      maxLines: 1,
+                                      style: Constants.smallTextWhite600,
+                                      textAlign: TextAlign.left,
+                                    ),
+                                    AutoSizeText(
+                                      'Registered since: ' +
+                                              authService
+                                                  .getRegistrationDateFormatted() ??
+                                          '',
+                                      maxLines: 1,
+                                      style: Constants.smallTextWhite600,
+                                      textAlign: TextAlign.left,
+                                    ),
+                                  ])),
+                        )),
+                    Expanded(
+                      flex: 2,
+                      child: Container(
+                        padding: EdgeInsets.only(top: 8),
+                        child: RawMaterialButton(
+                            onPressed: () => {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            UserSettingsScreen()),
+                                  )
+                                },
+                            elevation: 2.0,
+                            child: Icon(
+                              Icons.settings_rounded,
+                              size: 30,
+                              color: Colors.grey,
+                            ),
+                            shape: CircleBorder()),
+                      ),
+                    ),
+                  ]),
+              Padding(
+                padding: const EdgeInsets.only(right: 8.0),
                 child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Row(children: <Widget>[
-                        Expanded(
-                            flex: 3,
-                            child: Container(
-                                alignment: Alignment.centerLeft,
-                                child: CircleAvatar(
-                                    minRadius: 16.0,
-                                    maxRadius: 32.0,
-                                    //backgroundImage: NetworkImage(appUser.imageUrl),
-                                    child: AutoSizeText(
-                                        _getUserInitials(appUser.displayName),
-                                        maxLines: 1,
-                                        style: Constants.subHeaderTextWhite)))),
-                        Expanded(
-                            flex: 7,
-                            child: FittedBox(
-                                fit: BoxFit.fitWidth,
-                                child: Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: <Widget>[
-                                      Row(
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.spaceBetween,
-                                          children: <Widget>[
-                                            AutoSizeText(
-                                              appUser.displayName ?? '',
-                                              maxLines: 1,
-                                              style: Constants.headerTextWhite,
-                                              textAlign: TextAlign.left,
-                                            ),
-                                            FittedBox(
-                                                fit: BoxFit.fitWidth,
-                                                child: IconButton(
-                                                  icon: Icon(
-                                                    Icons.settings_rounded,
-                                                    color: Colors.grey,
-                                                  ),
-                                                  onPressed: () {
-                                                    Navigator.push(
-                                                      context,
-                                                      MaterialPageRoute(
-                                                          builder: (context) =>
-                                                              UserSettingsScreen()),
-                                                    );
-                                                  },
-                                                ))
-                                          ]),
-                                      AutoSizeText(
-                                        appUser.email ?? '',
-                                        maxLines: 1,
-                                        style: Constants.smallTextWhite600,
-                                        textAlign: TextAlign.left,
-                                      ),
-                                      AutoSizeText(
-                                        'Registered since: ' +
-                                                authService
-                                                    .getRegistrationDateFormatted() ??
-                                            '',
-                                        maxLines: 1,
-                                        style: Constants.smallTextWhite600,
-                                        textAlign: TextAlign.left,
-                                      ),
-                                    ])))
-                      ]),
+                  children: <Widget>[
+                    Divider(
+                      color: Colors.white24,
+                      thickness: 2,
+                      height: 20,
+                    ),
+                    Row(children: <Widget>[
+                      Expanded(
+                          flex: 3,
+                          child: AutoSizeText(
+                            'Gym:',
+                            maxLines: 1,
+                            style: Constants.headerTextWhite,
+                            textAlign: TextAlign.left,
+                          )),
+                      Expanded(
+                          flex: 7,
+                          child: FutureBuilder<String>(
+                              future: _getGymNameById(appUser.selectedGym),
+                              builder: (context, gymSnapshot) {
+                                return AutoSizeText(
+                                  gymSnapshot.data ?? '',
+                                  maxLines: 1,
+                                  style: Constants.headerTextWhite,
+                                  textAlign: TextAlign.right,
+                                );
+                              })),
+                    ]),
+                    if (_getPrivileges(appUser))
                       Divider(
                         color: Colors.white24,
                         thickness: 2,
                         height: 20,
                       ),
+                    if (_getPrivileges(appUser))
                       Row(children: <Widget>[
                         Expanded(
                             flex: 3,
                             child: AutoSizeText(
-                              'Gym:',
+                              'Permission:',
                               maxLines: 1,
                               style: Constants.headerTextWhite,
                               textAlign: TextAlign.left,
                             )),
                         Expanded(
                             flex: 7,
-                            child: FutureBuilder<String>(
-                                future: _getGymNameById(appUser.selectedGym),
-                                builder: (context, gymSnapshot) {
-                                  return AutoSizeText(
-                                    gymSnapshot.data ?? '',
-                                    maxLines: 1,
-                                    style: Constants.headerTextWhite,
-                                    textAlign: TextAlign.right,
-                                  );
-                                })),
-                      ]),
-                      if (_getPrivileges(appUser))
-                        Divider(
-                          color: Colors.white24,
-                          thickness: 2,
-                          height: 20,
-                        ),
-                      if (_getPrivileges(appUser))
-                        Row(children: <Widget>[
-                          Expanded(
-                              flex: 3,
-                              child: AutoSizeText(
-                                'Permission:',
-                                maxLines: 1,
-                                style: Constants.headerTextWhite,
-                                textAlign: TextAlign.left,
-                              )),
-                          Expanded(
-                              flex: 7,
-                              child: AutoSizeText(
-                                _getUserRoleForCurrentGym(appUser) ?? '',
-                                maxLines: 1,
-                                style: Constants.headerTextWhite,
-                                textAlign: TextAlign.right,
-                              ))
-                        ])
-                    ]))));
+                            child: AutoSizeText(
+                              _getUserRoleForCurrentGym(appUser) ?? '',
+                              maxLines: 1,
+                              style: Constants.headerTextWhite,
+                              textAlign: TextAlign.right,
+                            ))
+                      ])
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 
   bool _getPrivileges(AppUser user) {
